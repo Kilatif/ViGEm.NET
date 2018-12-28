cd6df34b8f11fc8a55b3a427c9fa0bccae151c1a
Vitaly Kolesnikov <kilatif1992@gmail.com>
2018-12-28 04:12:49 +0300

    Start implementation joyconprodevice class

---
+++ ViGEmClient.Net.Demo/JoyConDevice.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HidSharp;
+using HidSharp.Reports.Input;
+
+namespace ViGEmClient.Net.Demo
+{
+    class JoyConDevice : IDisposable
+    {
+        private HidDevice _hidDevice;
+        private HidStream _hidStream;
+        private HidDeviceInputReceiver _inputReceiver;
+        private byte[] _reportBuffer;
+
+        public event Action<byte[]> InputReportRecived; 
+
+        public bool InitializeDevice(int productId)
+        {
+            var list = DeviceList.Local;
+            var hidDeviceList = list.GetHidDevices().ToArray();
+            _hidDevice = hidDeviceList.FirstOrDefault(dev => dev.ProductID == productId);
+
+            if (_hidDevice == null)
+            {
+                return false;
+            }
+
+            var isSuccess = _hidDevice.TryOpen(out _hidStream);
+            if (!isSuccess)
+            {
+                return false;
+            }
+
+            _hidStream.ReadTimeout = Timeout.Infinite;
+
+            _reportBuffer = new byte[_hidDevice.GetMaxInputReportLength()];
+            _inputReceiver = _hidDevice.GetReportDescriptor().CreateHidDeviceInputReceiver();
+            _inputReceiver.Received += OnReportReceived;
+
+            return true;
+        }
+
+        public void SendOutputReport(byte[] report)
+        {
+            _hidStream.Write(report);
+        }
+
+        private void OnReportReceived(object sender, EventArgs args)
+        {
+            Array.Clear(_reportBuffer, 0, _reportBuffer.Length);
+
+            var offset = 0;
+            while (_inputReceiver.TryRead(_reportBuffer, offset, out var report))
+            {
+                offset += report.Length;
+            }
+
+            InputReportRecived?.Invoke(_reportBuffer);
+        }
+
+        public void Dispose()
+        {
+            if (_inputReceiver != null)
+            {
+                _inputReceiver.Received -= OnReportReceived;
+            }
+
+            _hidStream?.Dispose();
+        }
+    }
+}
---
+++ ViGEmClient.Net.Demo/JoyConsProDevice.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidSharp;
+using HidSharp.Reports.Input;
+
+namespace ViGEmClient.Net.Demo
+{
+    enum MainJoyCon
+    {
+        Left, Right
+    }
+
+    enum ErrorCode
+    {
+        NoError,
+        LeftJoyConInitializeError,
+        RightJoyConInitializeError,
+    }
+
+    class JoyConsProDevice : IDisposable
+    {
+        private const int LeftJoyConProductId = 0x2006;
+        private const int RightJoyConProductId = 0x2007;
+
+        private JoyConDevice _leftJoyCon;
+        private JoyConDevice _rightJoyCon;
+
+        private MainJoyCon _mainJoyCon;
+
+        public ErrorCode Initialize()
+        {
+            _leftJoyCon = new JoyConDevice();
+            _rightJoyCon = new JoyConDevice();
+
+            if (!_leftJoyCon.InitializeDevice(LeftJoyConProductId))
+            {
+                return ErrorCode.LeftJoyConInitializeError;
+            }
+
+            if (!_rightJoyCon.InitializeDevice(LeftJoyConProductId))
+            {
+                return ErrorCode.RightJoyConInitializeError;
+            }
+
+            _leftJoyCon.InputReportRecived += OnLeftJoyConInputReceived;
+            _rightJoyCon.InputReportRecived += OnRightJoyConInputReceived;
+
+            return ErrorCode.NoError;
+        }
+
+        private void OnLeftJoyConInputReceived(byte[] report)
+        {
+            
+        }
+
+        private void OnRightJoyConInputReceived(byte[] report)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            _leftJoyCon.Dispose();
+            _rightJoyCon.Dispose();
+        }
+    }
+}
